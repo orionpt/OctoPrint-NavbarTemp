4a5b664e45977dc3f0eb627f5e43f9554cc7b47c
ntoff <norahtihtzoff@gmail.com>
2017-12-03 17:13:42 +1000

    Make compatible with 1.3.6

--- octoprint_navbartemp/static/js/navbartemp.js
+++ octoprint_navbartemp/static/js/navbartemp.js
@@ -2,53 +2,52 @@ $(function() {
     function NavbarTempViewModel(parameters) {
         var self = this;
 
-        self.navBarTempModel = parameters[0];
+        self.temperatureModel = parameters[0];
         self.global_settings = parameters[1];
-        self.raspiTemp = ko.observable();
-        self.isRaspi = ko.observable(false);
-		self.isAwinner = ko.observable(false);
-
+        self.socTemp = ko.observable(null);
+        /* 
+         * raspi and awinner should be combined into something like hasSoc in the python
+         * source, there's no need for this part to know or care what the sbc is made of
+         * 
+         */
+        self.isRaspi = ko.observable(false); 
+        self.isAwinner = ko.observable(false);
+        //hassoc should be taken care of in the python source before it gets this far
+        self.hasSoc = ko.pureComputed(function() {
+            return self.isRaspi || self.isAwinner;
+        });
+        
         self.onBeforeBinding = function () {
             self.settings = self.global_settings.settings.plugins.navbartemp;
         };
 
+        self.formatBarTemperature = function(toolName, actual, target) {
+            var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
+        
+            if (target) {
+                var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
+                output += sign + _.sprintf("%.1f&deg;C", target);
+            }
+        
+            return output;
+        };
+        
         self.onDataUpdaterPluginMessage = function(plugin, data) {
             if (plugin != "navbartemp") {
                 return;
             }
-
-            if (!data.hasOwnProperty("israspi")) {
-                self.isRaspi(false);
-                return;
-            } else {
-                self.isRaspi(true);
+            else {
+                self.isRaspi(data.israspi);
+                self.isAwinner(data.isawinner);
+                self.socTemp(_.sprintf("SoC: %.1f&deg;C", data.raspitemp));
             }
-			
-			if (!data.hasOwnProperty("isawinner")) {
-                self.isAwinner(false);
-                return;
-            } else {
-                self.isAwinner(true);
-            }
-
-            self.raspiTemp(_.sprintf("SoC: %.1f&deg;C", data.raspitemp));
         };
     }
 
-    ADDITIONAL_VIEWMODELS.push([
-        NavbarTempViewModel, 
-        ["temperatureViewModel", "settingsViewModel"],
-        ["#navbar_plugin_navbartemp", "#settings_plugin_navbartemp"]
-    ]);
+    OCTOPRINT_VIEWMODELS.push({
+        construct: NavbarTempViewModel, 
+        dependencies: ["temperatureViewModel", "settingsViewModel"],
+        elements: ["#navbar_plugin_navbartemp", "#settings_plugin_navbartemp",]
+    });
 });
 
-function formatBarTemperature(toolName, actual, target) {
-    var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
-
-    if (target) {
-        var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
-        output += sign + _.sprintf("%.1f&deg;C", target);
-    }
-
-    return output;
-};
